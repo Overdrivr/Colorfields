c545092ab7fac40fd2317bea65b90d72234b7b56
Rémi Bèges <remi.beges@gmail.com>
2015-05-25 16:22:30 +0200

    Cleanup
    In preparation of level loader

--- core/src/com/mygdx/game/GameEngine.java
+++ core/src/com/mygdx/game/GameEngine.java
@@ -34,7 +34,7 @@ public class GameEngine {
     public World world;
     private float accumulator = 0;
     private Vector<Body> spheres;
-    Vector2 sunPosition;
+    //Vector2 sunPosition;
     Vector2 cannonPosition;
 
     b2Separator splitter;
@@ -55,6 +55,13 @@ public class GameEngine {
     //debug objects
     ShapeRenderer renderer;
 
+    // Level properties
+    float worldSize;
+    float camerabound_plus_x;
+    float camerabound_minus_x;
+    float camerabound_plus_y;
+    float camerabound_minus_y;
+
     public GameEngine(final Stage s){
         stage = s;
 
@@ -65,12 +72,26 @@ public class GameEngine {
         assetManager = new AssetManager();
         renderer = new ShapeRenderer();
 
-        initPhysics();
-
         initLevel();
     }
 
     private void initLevel(){
+        // Level properties
+        worldSize = 5000.f;
+        camerabound_plus_x = worldSize/2.f;
+        camerabound_minus_x = -worldSize/2.f;
+        camerabound_plus_y = worldSize/2.f;
+        camerabound_minus_y = -worldSize/2.f;
+        cannonPosition = new Vector2(-400.f,10.f);
+
+        // Init physics world
+        world = new World(new Vector2(0.f,0.f),true);
+
+        spheres = new Vector();
+
+        createStartPoint(new Vector2(-400, 1));
+
+
         // Grid
         field = new GravityField(new Vector2(-500,-500),50,20.f);
         field.addSphericalAttractor(new Vector2(-200,-200));
@@ -107,103 +128,59 @@ public class GameEngine {
 
     private void updateFields(){
         //Update gravity forces
-
         for(Body b : spheres){
-            //Compute vector from center of body to center of sun
-            /*Vector2 v = b.getPosition();
-            float r = v.dst(sunPosition);
-            v.x = sunPosition.x - v.x;
-            v.y = sunPosition.y - v.y;
-            //Normalize and apply gravity force toward the sun
-            float force = 1000000.f / (r * r);
-
-            v.setLength(force);
-            //Gdx.app.log("Force",Float.toString(v.x)+" "+Float.toString(v.y));
-            //Apply force to each body
-            b.applyForce(v, b.getWorldCenter(), true);*/
             Vector2 force = field.getForce(b.getPosition().x,b.getPosition().y);
             b.applyForce(force,b.getPosition(),true);
         }
     }
 
-    private void initPhysics(){
-        //PHYSICS
-        world = new World(new Vector2(0.f,0.f),true);
-        sunPosition = new Vector2(-400.f,90.f);
-        cannonPosition = new Vector2(-400.f,10.f);
+    private void createStartPoint(Vector2 position){
+        // Init the starting point of thrown objects (ore)
 
-        spheres = new Vector();
-
-        // Create our body definition
         BodyDef groundBodyDef = new BodyDef();
-        // Set its world position
-        groundBodyDef.position.set(new Vector2(-400, 1));
+        groundBodyDef.position.set(position);
 
-        // Create a body from the definition and add it to the world
         Body groundBody = world.createBody(groundBodyDef);
 
-        // Create a polygon shape
         PolygonShape groundBox = new PolygonShape();
-        // Set the polygon shape as a box which is twice the size of our view port and 20 high
-        // (setAsBox takes half-width and half-height as arguments)
         groundBox.setAsBox(10.f, 1.0f);
 
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = groundBox;
         fixtureDef.isSensor = true;
-
-        // Create a fixture from our polygon shape and add it to our ground body
         groundBody.createFixture(fixtureDef);
-        // Clean up after ourselves
+
         groundBox.dispose();
-
-        //Create a sun
-        BodyDef sunBodyDef = new BodyDef();
-        sunBodyDef.position.set(sunPosition);
-        Body sunBody = world.createBody(sunBodyDef);
-        CircleShape sunShape = new CircleShape();
-        sunShape.setRadius(5.0f);
-        sunBody.createFixture(sunShape, 0.0f);
-        sunShape.dispose();
-
-        //test aera
-        //testarea();
     }
 
     public void createSphere(float x, float y)
     {
-        // First we create a body definition
+        // Create a new ore to throw from starting point
+
         BodyDef bodyDef = new BodyDef();
-        // We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
         bodyDef.type = BodyDef.BodyType.DynamicBody;
-        // Set our body's starting position in the world
         bodyDef.position.set(cannonPosition);
 
-        // Create our body in the world using our body definition
         Body body = world.createBody(bodyDef);
 
-        // Create a circle shape and set its radius to 6
         CircleShape circle = new CircleShape();
         circle.setRadius(2.5f);
 
         spheres.add(body);
 
-        // Create a fixture definition to apply our shape to
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = circle;
         fixtureDef.density = 0.5f;
         fixtureDef.friction = 5.0f;
-        fixtureDef.restitution = 0.6f; // Make it bounce a little bit
+        fixtureDef.restitution = 0.6f;
 
-
-        // Create our fixture and attach it to the body
         Fixture fixture = body.createFixture(fixtureDef);
 
-        //Compute shooting vector
+        // Compute shooting vector
         Vector2 shootingVector = new Vector2();
         shootingVector.x = x - cannonPosition.x;
         shootingVector.y = y - cannonPosition.y;
-        shootingVector.setLength(1000.f);//400
+        shootingVector.setLength(1000.f);
 
         body.applyLinearImpulse(shootingVector, cannonPosition, true);
 
