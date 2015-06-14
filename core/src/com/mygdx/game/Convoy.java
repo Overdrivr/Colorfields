dc757cd5fb992496ddf1d241466911ab47f10468
Rémi Bèges <remi.beges@gmail.com>
2015-06-14 22:05:23 +0200

    Working on improved joint
    For #12
    But current strategy with two distance joints is not appropriate because
    it provides too much degrees of freedom
    Instead a single revolution joint with a distance joint with low
    frequency should provide the desired behavior

--- core/src/com/mygdx/game/Convoy.java
+++ core/src/com/mygdx/game/Convoy.java
@@ -1,11 +1,13 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.joints.DistanceJointDef;
 
 import java.util.Vector;
@@ -17,6 +19,15 @@ public class Convoy {
     private final GameEngine engine;
     private Vector<Body> containers;
 
+    // Constants
+    float jointFrequency = 3f;
+    float jointLength = 0.3f;
+    float jointOffsetx = 0.05f;// Must be <= than containerx
+    float jointOffsety = 0.1f;// Must be <= than containery
+    float containerx = 0.05f;
+    float containery = 0.1f;
+
+
     public Convoy(GameEngine e, Vector2 position, Vector2 orientation, Vector2 force, int amount) {
         engine = e;
         containers = new Vector<Body>();
@@ -25,24 +36,42 @@ public class Convoy {
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
         bodyDef.position.set(position);
+        bodyDef.angle = 0;
         Body body = e.world.createBody(bodyDef);
 
         MyBodyData data = new MyBodyData();
         data.type = BodyType.BODY_TYPE_ORE;
         body.setUserData(data);
 
-        CircleShape circle = new CircleShape();
-        circle.setRadius(0.05f);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(containerx,containery);
 
         FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = circle;
+        fixtureDef.shape = shape;
         fixtureDef.density = 0.5f;
         fixtureDef.friction = 5.0f;
         fixtureDef.restitution = 0.6f;
-        body.createFixture(fixtureDef);
+        Fixture f = body.createFixture(fixtureDef);
         body.applyLinearImpulse(force, position, true);
 
         containers.add(body);
+
+        orientation.setLength(jointLength);
+
+
+        // Lateral offsets of joints in container ref
+        /*Vector2 offsetA1 = ortho(orientation,true);
+        Vector2 offsetA2 = ortho(orientation,false);
+
+        offsetA1.setLength(jointOffsetx);
+        offsetA2.setLength(jointOffsetx);
+
+        // Vertical offsets of joints in container ref
+        Vector2 offsetB1 = orientation;
+        Vector2 offsetB2 = orientation;
+        offsetB1.setLength(containery);
+        offsetB2.setLength(-containery);*/
+
 
         //All following spheres
         for(int i = 0 ; i < amount ; i++){
@@ -50,18 +79,39 @@ public class Convoy {
             Body body2 = e.world.createBody(bodyDef);
             Fixture f2 = body2.createFixture(fixtureDef);
 
-            //Joint
+            //left joint
             DistanceJointDef jointDef = new DistanceJointDef();
-            jointDef.frequencyHz = 1.5f;
-            jointDef.initialize(containers.lastElement(), body2, containers.lastElement().getPosition(), body2.getPosition());
+            jointDef.frequencyHz = jointFrequency;
+            jointDef.length = jointLength;
+
+            Vector2 jointStart = new Vector2();
+            jointStart.x = containers.lastElement().getPosition().x + containerx;
+            jointStart.y = containers.lastElement().getPosition().y + containery;
+
+            Vector2 jointEnd = new Vector2();
+            jointEnd.x = body2.getPosition().x + containerx;
+            jointEnd.y = body2.getPosition().y - containery;
+
+            jointDef.initialize(containers.lastElement(), body2, jointStart, jointEnd);
             e.world.createJoint(jointDef);
 
+            // Right joint
+            jointStart.x = containers.lastElement().getPosition().x - containerx;
+            jointStart.y = containers.lastElement().getPosition().y + containery;
+
+            jointEnd.x = body2.getPosition().x - containerx;
+            jointEnd.y = body2.getPosition().y - containery;
+
+            jointDef.initialize(containers.lastElement(), body2, jointStart, jointEnd);
+            e.world.createJoint(jointDef);
+
+            // Apply initial impulse to the body
             body2.applyLinearImpulse(force, body2.getPosition(), true);
 
             containers.add(body2);
         }
 
-        circle.dispose();
+        shape.dispose();
     }
 
     public void update(GravityField field){
@@ -69,5 +119,16 @@ public class Convoy {
             Vector2 force = field.getForce(c.getPosition().x,c.getPosition().y);
             c.applyForce(force,c.getPosition(),true);
         }
+    }
+
+    private Vector2 ortho(Vector2 a, boolean left){
+        Vector3 u = new Vector3(a.x,a.y,0);
+        Vector3 v = left ? new Vector3(0,0,1) : new Vector3(0,0,-1);
+
+        Vector2 result = new Vector2();
+        result.x = u.y * v.z - u.z * v.y;
+        result.y = u.z * v.x - u.x * v.z;
+
+        return result;
     }
 }
